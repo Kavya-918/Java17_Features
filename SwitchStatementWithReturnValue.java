47aa9ea8eec91440aa04a61976118eb0ca428043
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-11-01 16:22:32 +0530

    Add files via upload

---
+++ SwitchStatementWithReturnValue.java
@@ -0,0 +1,34 @@
+public class SwitchStatementWithReturnValue {
+
+    // Method 1: Switch Expression returning String based on Object type
+    static String formatter(Object obj) {
+        return switch (obj) {
+            case Integer i -> "Integer: " + i;
+            case String s  -> "String: " + s;
+            case null      -> "Null Value";
+            default        -> "Unknown Type";
+        };
+    }
+
+    // Method 2: Switch Expression with Number classification
+    static String classifyNumber(Number number) {
+        return switch (number) {
+            case Integer i -> i % 2 == 0 ? "Even Integer" : "Odd Integer";
+            case Double d  -> d > 0 ? "Positive Double" : "Non-positive Double";
+            default -> "Unknown Number";
+        };
+    }
+
+    // Main method to test both
+    public static void main(String[] args) {
+        System.out.println(formatter(10));
+        System.out.println(formatter("Kavyadeveloper"));
+        System.out.println(formatter(null));
+        System.out.println(formatter(3.14));
+
+        System.out.println(classifyNumber(10));
+        System.out.println(classifyNumber(7));
+        System.out.println(classifyNumber(3.14));
+        System.out.println(classifyNumber(-2.5));
+    }
+}
