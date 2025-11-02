2db6e106618bb6342b5dc1f615faec4ff7d92b87
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-11-02 13:46:01 +0530

    Update TextBlockDemo.java

--- TextBlockDemo.java
+++ TextBlockDemo.java
@@ -21,3 +21,25 @@ public class TextBlockDemo {
         System.out.println("\nAfter Text Block:\n" + jsonNew);
     }
 }
+
+/**
+"C:\Program Files\Java\jdk-17.0.4\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.3.5\lib\idea_rt.jar=59012" -Dfile.encoding=UTF-8 -classpath D:\Java17\out\production\Java17 TextBlockDemo
+Before Text Block:
+{
+    "name": "KavyaDeveloper",
+    "project": "BugTracker System",
+    "role": "Developer"
+}
+
+After Text Block:
+{
+    "name": "KavyaDeveloper",
+    "project": "BugTracker System",
+    "role": "Developer"
+}
+
+
+Process finished with exit code 0
+
+
+**/
